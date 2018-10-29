35116fe84fb37c9a611f898923205d2517e251d2
Adim Victor <adimvicky@gmail.com>
2018-10-28 20:31:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express'),
       bodyParser = require('body-parser'),
       mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mongomovies');
+mongoose.connect('mongodb://localhost/moviedb'); 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Connection Error'));
